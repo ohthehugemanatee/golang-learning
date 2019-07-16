d08f62f1dca1e422ab030f593e3d612662c69ad6
Campbell Vertesi <campbell@vertesi.com>
2019-07-16 21:22:30 +0200

    Remove unnecessary test

--- integers/sum_test.go
+++ integers/sum_test.go
@@ -3,15 +3,6 @@ package integers
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("slice of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-		sum := Sum(numbers)
-		expected := 15
-		if sum != expected {
-			t.Errorf("expected '%d' but got '%d', gave %v", expected, sum, numbers)
-		}
-	})
-
 	t.Run("slice of 3 numbers", func(t *testing.T) {
 		numbers := []int{5, 6, 7}
 		sum := Sum(numbers)
