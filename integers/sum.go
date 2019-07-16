4b1dddc656fab675e1d5ee9676f58ce479c48585
Campbell Vertesi <campbell@vertesi.com>
2019-07-16 21:10:51 +0200

    Change sum to use array of numbers

--- integers/sum.go
+++ integers/sum.go
@@ -1,5 +1,9 @@
 package integers
 
-func Sum(a, b int) int {
-	return a+b
+// Sum adds an array of 5 numbers together.
+func Sum(numbers [5]int) (sum int) {
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return
 }
--- integers/sum_test.go
+++ integers/sum_test.go
@@ -3,8 +3,9 @@ package integers
 import "testing"
 
 func TestSum(t *testing.T) {
-	sum := Sum(3, 4)
-	expected := 7
+	numbers := [5]int{1, 2, 3, 4, 5}
+	sum := Sum(numbers)
+	expected := 15
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
