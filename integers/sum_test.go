798b58414b2b5a5f5de04e74c4a7c7700be63cfd
Campbell Vertesi <campbell@vertesi.com>
2019-07-16 23:47:25 +0200

    Minor refactor in tests

--- integers/sum_test.go
+++ integers/sum_test.go
@@ -25,18 +25,21 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	t.Run("Basic set of arrays to tailSum", func(t *testing.T) {
-		result := SumAllTails([]int{1, 2, 3, 4}, []int{5, 6}, []int{7, 8, 9})
-		expected := []int{9, 6, 17}
+	checkResult := func(result, expected []int, t *testing.T) {
 		if !reflect.DeepEqual(result, expected) {
 			t.Errorf("Expected %v, got %v", expected, result)
 		}
+	}
+
+	t.Run("Basic set of arrays to tailSum", func(t *testing.T) {
+		result := SumAllTails([]int{1, 2, 3, 4}, []int{5, 6}, []int{7, 8, 9})
+		expected := []int{9, 6, 17}
+		checkResult(result, expected, t)
+
 	})
 	t.Run("Empty array to tailSum", func(t *testing.T) {
 		result := SumAllTails([]int{}, []int{1, 2, 3})
 		expected := []int{0, 5}
-		if !reflect.DeepEqual(result, expected) {
-			t.Errorf("Expected %v, got %v", expected, result)
-		}
+		checkResult(result, expected, t)
 	})
 }
