3c337cf1610a8e6d72d33b7da2a134d5f72fa24a
Campbell Vertesi <campbell@vertesi.com>
2019-07-16 21:12:37 +0200

    Use range instead of for

--- integers/sum.go
+++ integers/sum.go
@@ -2,8 +2,8 @@ package integers
 
 // Sum adds an array of 5 numbers together.
 func Sum(numbers [5]int) (sum int) {
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return
 }
