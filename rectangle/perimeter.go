f13613eea78faa37f442e2c36317680b3c964ce8
Campbell Vertesi <campbell@vertesi.com>
2019-07-17 21:06:46 +0200

    Change perimeter to rectangle, add area function/test

--- rectangle/perimeter.go
+++ rectangle/perimeter.go
@@ -1,6 +1,10 @@
-package perimeter
+package rectangle
 
 // Perimeter calculates the perimeter of a square given height and width.
 func Perimeter(height, width float64) float64 {
 	return 2 * (width + height)
+}
+
+func Area(height, width float64) float64 {
+	return height * width
 }
--- rectangle/perimeter_test.go
+++ rectangle/perimeter_test.go
@@ -1,4 +1,4 @@
-package perimeter
+package rectangle
 
 import (
 	"testing"
@@ -7,6 +7,15 @@ import (
 func TestPerimeter(t *testing.T) {
 	result := Perimeter(5.0, 6.3)
 	expected := 22.6
+
+	if result != expected {
+		t.Errorf("Expected %.2f, got %.2f", expected, result)
+	}
+}
+
+func TestArea(t *testing.T) {
+	result := Area(5.0, 6.3)
+	expected := 31.5
 
 	if result != expected {
 		t.Errorf("Expected %.2f, got %.2f", expected, result)
