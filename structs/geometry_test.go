8bb6bbf9b11cd20bdf16654c4f64f74ecf91583c
Campbell Vertesi <campbell@vertesi.com>
2019-07-22 13:37:59 +0200

    Refactor geo test with an interface

--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -15,22 +15,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("Got %.2f, expected %.2f", got, want)
+		}
+	}
 	t.Run("Rectangle area", func(t *testing.T) {
 		rectangle := Rectangle{5.0, 6.3}
-		result := rectangle.Area()
-		expected := 31.5
-
-		if result != expected {
-			t.Errorf("Expected %.2f, got %.2f", expected, result)
-		}
+		checkArea(t, rectangle, 31.5)
 	})
 	t.Run("Circle area", func(t *testing.T) {
 		circle := Circle{10}
-		result := circle.Area()
-		expected := 314.1592653589793
-
-		if result != expected {
-			t.Errorf("Expected %.2f, got %.2f", expected, result)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
---
+++ structs/shape.go
@@ -0,0 +1,6 @@
+package structs
+
+// Shape represents any geometrical shape.
+type Shape interface {
+	Area() float64
+}
