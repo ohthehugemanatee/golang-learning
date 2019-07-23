48f1e82d9e7288a033280e6eb1f7a6f3db704d1a
Campbell Vertesi <campbell@vertesi.com>
2019-07-23 12:26:08 +0200

    Refactor tests for readability

--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -16,18 +16,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 5.0, Height: 6.3}, want: 31.5},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Width: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 5.0, Height: 6.3}, hasArea: 31.5},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Width: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("Got %.2f, expected %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v Got %.2f, expected %.2f", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
