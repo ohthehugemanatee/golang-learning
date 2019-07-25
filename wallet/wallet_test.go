cfb17e1496fa498a9320be3e00c9939f17bb8512
Campbell Vertesi <campbell@vertesi.com>
2019-07-25 12:41:33 +0200

    Refactor tests for clarity

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -4,33 +4,34 @@ import "testing"
 
 // TestWallet tests the wallet struct.
 func TestWallet(t *testing.T) {
-	checkBalance := func(wallet Wallet, want Bitcoin, t *testing.T) {
+	assertBalance := func(wallet Wallet, want Bitcoin, t *testing.T) {
 		t.Helper()
 		got := wallet.Balance()
 		if want != got {
 			t.Errorf("Got %s, wanted %s", got, want)
 		}
 	}
+	assertError := func(err error, t *testing.T) {
+		t.Helper()
+		if err == nil {
+			t.Error("Expected an error but didn't get one")
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-		checkBalance(wallet, Bitcoin(10), t)
+		assertBalance(wallet, Bitcoin(10), t)
 	})
 	t.Run("Withdrawal", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
 		wallet.Withdraw(Bitcoin(4))
-		checkBalance(wallet, Bitcoin(6), t)
+		assertBalance(wallet, Bitcoin(6), t)
 	})
 	t.Run("Insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(3)
 		wallet := Wallet{balance: startingBalance}
 		err := wallet.Withdraw(Bitcoin(5))
-		if err == nil {
-			t.Error("Expected an overdrawn balance error but didn't get one")
-		}
-		if wallet.Balance() != startingBalance {
-			t.Errorf("Expected balance to stay the same (%s) but got %s", startingBalance, wallet.Balance())
-		}
-
+		assertError(err, t)
+		assertBalance(wallet, startingBalance, t)
 	})
 }
