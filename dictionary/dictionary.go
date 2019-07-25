5cc796f6c04555b046b9a2310fad6fe0b51a57e4
Campbell Vertesi <campbell@vertesi.com>
2019-07-25 13:14:38 +0200

    Refactor tests for clarity

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,6 +1,9 @@
 package dictionary
 
+// Dictionary of terms and definitions.
+type Dictionary map[string]string
+
 // Search a given dictionary for a word.
-func Search(dictionary map[string]string, key string) string {
-	return dictionary[key]
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -5,8 +5,8 @@ import "testing"
 func TestSearch(t *testing.T) {
 	given := "testEntry"
 	want := "This is a test entry"
-	dictionary := map[string]string{given: want}
-	got := Search(dictionary, given)
+	dictionary := Dictionary{given: want}
+	got := dictionary.Search(given)
 	if got != want {
 		t.Errorf("Got %q, wanted %q, given %q", got, want, given)
 	}
