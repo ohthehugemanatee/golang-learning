2c222376b258a7a211891c6334991fe757641b0a
Campbell Vertesi <campbell@vertesi.com>
2019-07-25 12:00:02 +0200

    Refactor tests for clarity

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -4,22 +4,21 @@ import "testing"
 
 // TestWallet tests the wallet struct.
 func TestWallet(t *testing.T) {
+	checkBalance := func(wallet Wallet, want Bitcoin, t *testing.T) {
+		t.Helper()
+		got := wallet.Balance()
+		if want != got {
+			t.Errorf("Got %s, wanted %s", got, want)
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-		got := wallet.Balance()
-		want := Bitcoin(10)
-		if got != want {
-			t.Errorf("Got %s, wanted %s", got, want)
-		}
+		checkBalance(wallet, Bitcoin(10), t)
 	})
 	t.Run("Withdrawal", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
 		wallet.Withdraw(Bitcoin(4))
-		got := wallet.Balance()
-		want := Bitcoin(6)
-		if got != want {
-			t.Errorf("Got %s, wanted %s", got, want)
-		}
+		checkBalance(wallet, Bitcoin(6), t)
 	})
 }
