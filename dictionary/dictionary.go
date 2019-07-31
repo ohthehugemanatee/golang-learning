5022e6908c7f4c2d917c1f918d810bd10f9e4a9b
Campbell Vertesi <campbell@vertesi.com>
2019-07-31 15:23:56 +0200

    Return an error if no word is found

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,9 +1,17 @@
 package dictionary
 
+import (
+	"errors"
+)
+
 // Dictionary of terms and definitions.
 type Dictionary map[string]string
 
 // Search a given dictionary for a word.
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -7,9 +7,22 @@ func TestSearch(t *testing.T) {
 		word := "testEntry"
 		definition := "This is a test entry"
 		dictionary := Dictionary{word: definition}
-		got := dictionary.Search(word)
+		got, _ := dictionary.Search(word)
 		if got != definition {
 			t.Errorf("Got %q, wanted %q, given %q", got, definition, word)
 		}
 	})
+	t.Run("Nonexistent entry", func(t *testing.T) {
+		dictionary := Dictionary{}
+		_, err := dictionary.Search("test")
+		assertError(err, t)
+
+	})
+}
+
+func assertError(got error, t *testing.T) {
+	t.Helper()
+	if got.Error() == "" {
+		t.Errorf("Did not get an error where one was expected")
+	}
 }
