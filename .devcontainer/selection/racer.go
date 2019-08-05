99f46c44581a465010eba009ff1a46d9853b97c3
Campbell Vertesi <campbell@vertesi.com>
2019-08-05 17:08:57 +0200

    Add timeout

--- .devcontainer/selection/racer.go
+++ .devcontainer/selection/racer.go
@@ -1,22 +1,28 @@
 package selection
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
 // RaceWebsites compares the response time of 2 urls
-func RaceWebsites(urls [2]string) string {
-	speed1 := raceWebsite(urls[0])
-	speed2 := raceWebsite(urls[1])
-	if speed1 > speed2 {
-		return urls[1]
+func RaceWebsites(urls [2]string) (string, error) {
+	select {
+	case <-raceWebsite(urls[0]):
+		return urls[0], nil
+	case <-raceWebsite(urls[1]):
+		return urls[1], nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("All sites timed out")
 	}
-	return urls[0]
 }
 
-func raceWebsite(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func raceWebsite(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
--- .devcontainer/selection/racer_test.go
+++ .devcontainer/selection/racer_test.go
@@ -8,22 +8,36 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	fastServer := createDelayedServer(0 * time.Millisecond)
-	defer fastServer.Close()
-	slowServer := createDelayedServer(2 * time.Millisecond)
-	defer slowServer.Close()
+	t.Run("basic functionality", func(t *testing.T) {
+		fastServer := createDelayedServer(0 * time.Millisecond)
+		defer fastServer.Close()
+		slowServer := createDelayedServer(2 * time.Millisecond)
+		defer slowServer.Close()
 
-	fastURL := fastServer.URL
-	slowURL := slowServer.URL
-	urls := [2]string{fastURL, slowURL}
+		fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		urls := [2]string{fastURL, slowURL}
 
-	want := fastURL
-	got := RaceWebsites(urls)
+		want := fastURL
+		got, _ := RaceWebsites(urls)
 
-	if got != want {
-		t.Errorf("Returned the wrong URL. Expected %q, got %q", want, got)
-	}
+		if got != want {
+			t.Errorf("Returned the wrong URL. Expected %q, got %q", want, got)
+		}
+	})
+	t.Run("Timeout after 10 seconds", func(t *testing.T) {
+		timeoutServer := createDelayedServer(12 * time.Second)
+		defer timeoutServer.Close()
+		timeoutServer2 := createDelayedServer(11 * time.Second)
+		defer timeoutServer2.Close()
+		url1 := timeoutServer.URL
+		url2 := timeoutServer2.URL
 
+		_, err := RaceWebsites([2]string{url1, url2})
+		if err == nil {
+			t.Error("Expected an error but didn't get one")
+		}
+	})
 }
 
 func createDelayedServer(delay time.Duration) *httptest.Server {
