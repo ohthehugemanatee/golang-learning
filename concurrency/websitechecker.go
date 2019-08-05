a80de43522de312dabcb2d2329b8471cc0d0a972
Campbell Vertesi <campbell@vertesi.com>
2019-08-05 12:22:03 +0200

    Run urlchecker concurrently

--- concurrency/websitechecker.go
+++ concurrency/websitechecker.go
@@ -3,13 +3,25 @@ package concurrency
 // WebsiteChecker checks reachability of websites.
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 // CheckWebsites is the main entry function.
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
 	}
 
+	for i := 0; i < len(urls); i++ {
+		response := <-resultChannel
+		results[response.string] = response.bool
+	}
 	return results
 }
