e13728d973c2eab7bf535656f56449753e74cb43
Campbell Vertesi <campbell@vertesi.com>
2019-08-06 16:22:59 +0200

    Look for specific string value

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,6 +1,10 @@
 package reflection
 
+import "reflect"
+
 // Walk over an array, executing a function for each string member.
 func Walk(x interface{}, userFunc func(input string)) {
-
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	userFunc(field.String())
 }
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -3,10 +3,11 @@ package reflection
 import "testing"
 
 func TestWalk(t *testing.T) {
+	expected := "value1"
 	arr := struct {
 		string
 	}{
-		"value1",
+		expected,
 	}
 	got := []string{}
 	Walk(arr, func(str string) {
@@ -14,5 +15,8 @@ func TestWalk(t *testing.T) {
 	})
 	if len(got) != 1 {
 		t.Errorf("Function called the wrong number of times. Expected %d, got %d", 1, len(got))
+	}
+	if got[0] != expected {
+		t.Errorf("Wrong value in function call. Expected %s, got %s", expected, got[0])
 	}
 }
