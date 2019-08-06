a88f090f6318bba1ad6be26d258aa62000ddeecc
Campbell Vertesi <campbell@vertesi.com>
2019-08-06 19:16:34 +0200

    Refactor to reduce repetition

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,18 +5,22 @@ import "reflect"
 // Walk over an array, executing a function for each string member.
 func Walk(x interface{}, userFunc func(input string)) {
 	val := getValue(x)
+	numberOfValues := 0
+	var getField func(int) reflect.Value
 
 	switch val.Kind() {
-	case reflect.Slice:
-		for i := 0; i < val.Len(); i++ {
-			Walk(val.Index(i).Interface(), userFunc)
-		}
 	case reflect.String:
 		userFunc(val.String())
+	case reflect.Slice:
+		numberOfValues = val.Len()
+		getField = val.Index
 	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			Walk(val.Field(i).Interface(), userFunc)
-		}
+		numberOfValues = val.NumField()
+		getField = val.Field
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		Walk(getField(i).Interface(), userFunc)
 	}
 }
 
