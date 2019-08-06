f45d4da22d71185153ab3a6c7b6645f1ad7be8fc
Campbell Vertesi <campbell@vertesi.com>
2019-08-06 18:29:59 +0200

    Handle multiple string fields

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,6 +5,8 @@ import "reflect"
 // Walk over an array, executing a function for each string member.
 func Walk(x interface{}, userFunc func(input string)) {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	userFunc(field.String())
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		userFunc(field.String())
+	}
 }
